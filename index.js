e54938ebedd1093c219bc0949ca5b54f63f874fa
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-16 09:08:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function nowServing(line){
     return `Currently serving ${line.shift()}.`
   }
 }
+
+function currentLine(line){
+  if (line.length == 0){
+    return "The line is currently empty."
+  }
+}
